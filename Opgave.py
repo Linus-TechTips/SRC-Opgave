ca18969511c88e19c2174eb02d853535c7ac622e
linus vestergaard <linusbvestergaard@gmail.com>
2022-11-30 17:31:41 +0100

    Lavede nogle ændringer i koden

--- Opgave.py
+++ Opgave.py
@@ -4,7 +4,6 @@ import math
 BREDDE = 800
 HOEJDE = 600
 SPORLAENGDE = 500
-FARVE = arcade.csscolor.WHEAT
 
 def linje(tid, stationaert_punkt, retningsvektor):
     """Returnerer x- og y-koordinaterne til et punkt på en ret linje.
@@ -17,19 +16,19 @@ def linje(tid, stationaert_punkt, retningsvektor):
 
 def tegn(delta_tid):
     arcade.start_render()
-    # Første punkt for linjen
-    x, y = linje(tegn.tid, (0, 0), (90*math.cos(math.pi/6), 90*math.sin(math.pi/6)-0.5*9.82*tegn.tid))
-    x1, y1 = linje(tegn.tid, (0, 0), (90 * math.cos(math.pi/3), 90 * math.sin(math.pi/3) - 0.5 * 9.82 * tegn.tid))
-    x2, y2 = linje(tegn.tid, (0, 0), (90 * math.cos(45*math.pi / 180), 90 * math.sin(45*math.pi / 180) - 0.5 * 9.82 * tegn.tid))
-    x3, y3 = linje(tegn.tid, (0, 0), (90 * math.cos(75*math.pi/180), 90 * math.sin(75*math.pi/180) - 0.5 * 9.82 * tegn.tid))
-    x4, y4 = linje(tegn.tid, (0, 0), (90 * math.cos(15*math.pi/180), 90 * math.sin(15*math.pi/180) - 0.5 * 9.82 * tegn.tid))
-    # Tegner punktet på linjen
+    # Første punkt for linjerne
+    x, y = linje(tegn.tid, (0, 0), (120 * math.cos(math.pi/6), 120 * math.sin(math.pi/6) - 9.82 * tegn.tid))
+    x1, y1 = linje(tegn.tid, (0, 0), (120 * math.cos(math.pi/3), 120 * math.sin(math.pi/3) - 9.82 * tegn.tid))
+    x2, y2 = linje(tegn.tid, (0, 0), (120 * math.cos(45*math.pi/180), 120 * math.sin(45*math.pi/180) - 9.82 * tegn.tid))
+    x3, y3 = linje(tegn.tid, (0, 0), (120 * math.cos(75*math.pi/180), 120 * math.sin(75*math.pi/180) - 9.82 * tegn.tid))
+    x4, y4 = linje(tegn.tid, (0, 0), (120 * math.cos(15*math.pi/180), 120 * math.sin(15*math.pi/180) - 9.82 * tegn.tid))
+    # Tegner det første punkt på linjerne
     arcade.draw_circle_filled(x, y, 5, arcade.csscolor.BLACK)
     arcade.draw_circle_filled(x1, y1, 5, arcade.csscolor.WHITE)
     arcade.draw_circle_filled(x2, y2, 5 ,arcade.csscolor.PINK)
     arcade.draw_circle_filled(x3, y3, 5, arcade.csscolor.FOREST_GREEN)
     arcade.draw_circle_filled(x4, y4, 5, arcade.csscolor.ORANGE)
-    # Fjerner det første punkt i sporet, hvis sporet er for langt
+    # Fjerner det første punkt i sporene, hvis sporet er for langt
     if len(tegn.spor) > SPORLAENGDE:
         tegn.spor.pop(0)
     if len(tegn.spor2) > SPORLAENGDE:
@@ -40,7 +39,7 @@ def tegn(delta_tid):
         tegn.spor4.pop(0)
     if len(tegn.spor5) > SPORLAENGDE:
         tegn.spor5.pop(0)
-    # Tegner sporet  for linjen
+    # Tegner sporet for linjerne
     for punkt in tegn.spor:
         arcade.draw_circle_filled(*punkt, 2, arcade.csscolor.BLACK)
     for punkt in tegn.spor2:
