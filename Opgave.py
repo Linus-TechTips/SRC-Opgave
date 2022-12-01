c711b5ff85b5eb939460b98e739725557bf28590
linus vestergaard <linusbvestergaard@gmail.com>
2022-12-01 13:00:31 +0100

    ændret få ting

--- Opgave.py
+++ Opgave.py
@@ -6,8 +6,6 @@ HOEJDE = 600
 SPORLAENGDE = 500
 
 def linje(tid, stationaert_punkt, retningsvektor):
-    """Returnerer x- og y-koordinaterne til et punkt på en ret linje.
-    Er implementeret på helt samme måde, som i matematik."""
     x_0, y_0 = stationaert_punkt
     r_x, r_y = retningsvektor
     x = x_0 + r_x * tid
@@ -16,19 +14,19 @@ def linje(tid, stationaert_punkt, retningsvektor):
 
 def tegn(delta_tid):
     arcade.start_render()
-    # Første punkt for linjerne
+
     x, y = linje(tegn.tid, (0, 0), (120 * math.cos(math.pi/6), 120 * math.sin(math.pi/6) - 9.82 * tegn.tid))
     x1, y1 = linje(tegn.tid, (0, 0), (120 * math.cos(math.pi/3), 120 * math.sin(math.pi/3) - 9.82 * tegn.tid))
     x2, y2 = linje(tegn.tid, (0, 0), (120 * math.cos(45*math.pi/180), 120 * math.sin(45*math.pi/180) - 9.82 * tegn.tid))
     x3, y3 = linje(tegn.tid, (0, 0), (120 * math.cos(75*math.pi/180), 120 * math.sin(75*math.pi/180) - 9.82 * tegn.tid))
     x4, y4 = linje(tegn.tid, (0, 0), (120 * math.cos(15*math.pi/180), 120 * math.sin(15*math.pi/180) - 9.82 * tegn.tid))
-    # Tegner det første punkt på linjerne
+
     arcade.draw_circle_filled(x, y, 5, arcade.csscolor.BLACK)
     arcade.draw_circle_filled(x1, y1, 5, arcade.csscolor.WHITE)
     arcade.draw_circle_filled(x2, y2, 5 ,arcade.csscolor.PINK)
     arcade.draw_circle_filled(x3, y3, 5, arcade.csscolor.FOREST_GREEN)
     arcade.draw_circle_filled(x4, y4, 5, arcade.csscolor.ORANGE)
-    # Fjerner det første punkt i sporene, hvis sporet er for langt
+
     if len(tegn.spor) > SPORLAENGDE:
         tegn.spor.pop(0)
     if len(tegn.spor2) > SPORLAENGDE:
@@ -39,7 +37,7 @@ def tegn(delta_tid):
         tegn.spor4.pop(0)
     if len(tegn.spor5) > SPORLAENGDE:
         tegn.spor5.pop(0)
-    # Tegner sporet for linjerne
+
     for punkt in tegn.spor:
         arcade.draw_circle_filled(*punkt, 2, arcade.csscolor.BLACK)
     for punkt in tegn.spor2:
@@ -50,6 +48,7 @@ def tegn(delta_tid):
         arcade.draw_circle_filled(*punkt, 2, arcade.csscolor.FOREST_GREEN)
     for punkt in tegn.spor5:
         arcade.draw_circle_filled(*punkt, 2, arcade.csscolor.ORANGE)
+
     tegn.spor.append((x, y))
     tegn.spor2.append((x1, y1))
     tegn.spor3.append((x2, y2))
